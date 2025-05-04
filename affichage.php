07e8056d2c5cdae99b03fb2c73eb37789221abfc
Safaaaitelhaj <safaaitgst@gmail.com>
2025-05-04 19:48:53 +0200

    Update affichage.php

--- affichage.php
+++ affichage.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style2.css">
+   
 </head>
 <body>
 <?php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bind_param("ssissss", $nom, $prenom, $groupe, $sujet, $date_debut, $date_fin, $encadrement);
 
     if ($stmt->execute()) {
-        echo "<h3 style='color: green;'> Les informations ont été enregistrées avec succès dans BDD!</h3>";
+        echo "<h3 style='color: green;'> Les informations ont été enregistrées dans BDD!</h3>";
     } else {
         echo "<h3 style='color: red;'> Erreur lors de l'enregistrement : " . $stmt->error . "</h3>";
     }
@@ -45,7 +45,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
-
+<CENTER>
+    
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST['nom'];
@@ -57,16 +58,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $encadrement = $_POST['encadrement'];
     
     echo "<h2>Informations soumises :</h2>";
-    echo "<p><strong>Nom :</strong> " . $nom . "</p>";
-    echo "<p><strong>Prénom :</strong> " . $prenom . "</p>";
-    echo "<p><strong>Groupe :</strong> " . $group . "</p>";
-    echo "<p><strong>Sujet :</strong> " . $sujet . "</p>";
-    echo "<p><strong>Date début :</strong> " . $date_debut . "</p>";
-    echo "<p><strong>Date fin :</strong> " . $date_fin . "</p>";
-    echo "<p><strong>Encadrement :</strong> " . $encadrement . "</p>";
+    echo "<p><strong>Nom :</strong> " . $nom . "</p></br>";
+    echo "<p><strong>Prénom :</strong> " . $prenom . "</p></br>";
+    echo "<p><strong>Groupe :</strong> " . $group . "</p></br>";
+    echo "<p><strong>Sujet :</strong> " . $sujet . "</p></br>";
+    echo "<p><strong>Date début :</strong> " . $date_debut . "</p></br>";
+    echo "<p><strong>Date fin :</strong> " . $date_fin . "</p></br>";
+    echo "<p><strong>Encadrement :</strong> " . $encadrement . "</p></br>";
 } else {
     echo "<p>Aucune donnée soumise.</p>";
 }
 ?>
+</CENTER>
 </body>
 </html>
